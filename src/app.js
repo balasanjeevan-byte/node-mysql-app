084741ce7ac92b834eee54b0b0ec14e2c8f38aee
balasanjeevan-byte <balasanjeevan@gmail.com>
2026-01-08 23:35:59 +0530

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,1 +1,50 @@
+require("dotenv").config();
+const express = require("express");
+const db = require("./db");
+
+const app = express();
+app.use(express.json());
+
+const PORT = 3000;
+
+app.get("/", (req, res) => {
+  res.send("Node.js + MySQL App running 🚀");
+});
+
+app.get("/create-table", (req, res) => {
+  const sql = `
+    CREATE TABLE IF NOT EXISTS users (
+      id INT AUTO_INCREMENT PRIMARY KEY,
+      name VARCHAR(100),
+      email VARCHAR(100)
+    )
+  `;
+  db.query(sql, (err) => {
+    if (err) return res.status(500).send(err.message);
+    res.send("Users table created");
+  });
+});
+
+app.post("/add-user", (req, res) => {
+  const { name, email } = req.body;
+  db.query(
+    "INSERT INTO users (name, email) VALUES (?, ?)",
+    [name, email],
+    (err) => {
+      if (err) return res.status(500).send(err.message);
+      res.send("User added");
+    }
+  );
+});
+
+app.get("/users", (req, res) => {
+  db.query("SELECT * FROM users", (err, results) => {
+    if (err) return res.status(500).send(err.message);
+    res.json(results);
+  });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
 
